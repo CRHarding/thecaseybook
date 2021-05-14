309845dc95f3eb5c4c0a2877de74c146a999a2bd
unknown <casey.harding@generalassemb.ly>
2021-05-14 12:32:29 -0400

    User model in place.

---
+++ models/users.js
@@ -0,0 +1,12 @@
+const users = [
+	{
+		username: "CRHarding",
+		email: "c@c.com",
+		password: "casey123",
+		aboutMe: "Lorem ipsum dolor sit amet dolor lorem sit amet.",
+		img: "https://images.hindustantimes.com/rf/image_size_630x354/HT/p2/2020/06/24/Pictures/_dabbc3d4-b5de-11ea-b3b3-7b919605787e.jpg",
+		uuid: 12345
+	}
+]
+
+module.exports = users;
